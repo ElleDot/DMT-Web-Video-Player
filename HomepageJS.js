02eb0b60314ffa08007431509be8549a3373c8f4
Louis <louis.agars@gmail.com>
2019-01-04 16:52:32 +0000

    Updated Javascript
    Fullscreen now works :D
    Layout may be buggy
    
    final tweaks for upload soon.

--- HomepageJS.js
+++ HomepageJS.js
@@ -8,6 +8,15 @@ function goFullscreen() {
 	fullscreenNavOverlay.style.width = "100%";
 	videoPlayer.pause();
 	playButton.innerHTML = "&#9658; Play";
+	var currentSecond = videoPlayer.currentTime;
+	fullscreenVideoPlayer.currentTime = currentSecond;
+	fullscreenVideoPlayer.play();
+}
+function closeFullscreen() {
+	fullscreenNavOverlay.style.width = "0%";
+	fullscreenVideoPlayer.pause();
+	var currentSecond = fullscreenVideoPlayer.currentTime;
+	videoPlayer.currentTime = currentSecond;
 }
 function fullscreenCloseNav() {
 	fullscreenNavOverlay.style.width = "0%";
@@ -21,23 +30,46 @@ function chapterOneSkip() {
 	navOverlay.style.width = "0%";
 }
 function chapterTwoSkip() {
-	videoPlayer.currentTime = 10;
+	videoPlayer.currentTime = 35;
 	navOverlay.style.width = "0%";
 }
 function chapterThreeSkip() {
-	videoPlayer.currentTime = 20;
+	videoPlayer.currentTime = 76;
 	navOverlay.style.width = "0%";
 }				
 function chapterFourSkip() {
-	videoPlayer.currentTime = 30;
+	videoPlayer.currentTime = 157;
 	navOverlay.style.width = "0%";
 }		
 function creditsSkip() {
-	videoPlayer.currentTime = 40;
+	videoPlayer.currentTime = 221;
 	navOverlay.style.width = "0%";
 }
 
-//function goFullscreen() {
-	//var currentSecond = videoPlayer.currentTime;
-		//videoPlayer.currentTime = 20;
-//}
+function playPauseFunction() {
+	if (videoPlayer.paused) {
+		videoPlayer.play();
+		playButton.innerHTML = "&#10074&#10074; Pause";
+	} else {
+		videoPlayer.pause();
+		playButton.innerHTML = "&#9658; Play";
+	}	
+}
+function fullscreenPlayPauseFunction() {
+	if (fullscreenVideoPlayer.paused) {
+		fullscreenVideoPlayer.play();
+		fullscreenPlayButton.innerHTML = "&#10074&#10074;";
+	} else {
+		fullscreenVideoPlayer.pause();
+		fullscreenPlayButton.innerHTML = "&#9658;";
+	}	
+}			
+function muteUnmuteFunction() {
+	if (videoPlayer.muted) {
+		videoPlayer.muted = false;
+		muteButton.innerHTML = "&#128263; Mute Audio";
+	} else {
+		videoPlayer.muted = true;
+		muteButton.innerHTML = "&#128266; Unmute Audio";
+	}
+}
